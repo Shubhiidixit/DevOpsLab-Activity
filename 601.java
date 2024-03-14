39782108a7d40e933f16c2ecb4330b767efadf25
Deepika sharma <Deepikasharma8955@gmail.com>
2024-03-14 10:23:47 +0530

    Add files via upload

---
+++ 601.java
@@ -0,0 +1,8 @@
+class 601{
+    public static void main(String args[]){
+            int i= 2200;
+            if(i%4==0){ 
+      if(i%400==0){System.out.println(i+ " is a leap year");}
+      else{System.out.println(i + " is not a leap Year"); }}
+        }
+      }
