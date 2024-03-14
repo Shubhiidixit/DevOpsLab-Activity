b2f8207d0d1245da16e7ad9565f32d3a0acf415a
DrishtiSinha7 <drishtisinha70234@gmail.com>
2024-03-14 09:47:32 +0530

    Add LeapYear.java file

---
+++ LeapYear.java
@@ -0,0 +1,10 @@
+//Wap to find out whether 2200 is leap year or not by Drishti Sinha
+
+class LeapYear{
+ public static void main(String args[]){
+         int i= 2200;
+         if(i%4==0){ 
+   if(i%400==0){System.out.println(i+ " is a leap year");}
+   else{System.out.println(i + " is not a leap Year"); }}
+     }
+   }
